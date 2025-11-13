9b51b2a24e33ad77c1f14015536581e2c788afbf
Elijah Zimmerman <elijahz0409@gmail.com>
2025-11-13 10:16:00 -0500

    Implement basic pile struct

---
+++ src/deck.rs
@@ -0,0 +1,49 @@
+
+// A pile of an arbitrary number of cards
+struct Pile {
+    cards: Vec<Card>,
+    index: usize,
+}
+
+impl Pile {
+    fn new() -> Self {
+        Pile {
+            cards: Vec::new(),
+            index: 0,
+        }
+    }
+
+    // put card on top of pile
+    fn place_top(&mut self, card: Card) {
+        self.index += 1;
+        self.cards.push(card);
+    }
+
+    // put card on bottom of pile
+    fn place_bottom(&mut self, card: Card) {
+        self.index += 1;
+        self.cards.insert(0, card);
+    }
+
+    // draw card from top of pile
+    fn draw(&mut self) -> Card {
+        self.index -= 1;
+        self.cards.pop().unwrap()
+    }
+}
+
+impl Iterator for Pile {
+    type Item = Card;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.index <= 0 {
+            return None;
+        }
+        self.index -= 1;
+        self.cards.get(self.index).map(|c| *c)
+    }
+}
+
+#[derive(Copy, Clone)]
+// temporary
+struct Card {}
