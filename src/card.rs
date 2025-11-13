12d7e5c9fb53b3bca99986f418c4400af76eafbd
Elijah Zimmerman <elijahz0409@gmail.com>
2025-11-13 10:58:07 -0500

    Implement basic card structure

--- src/card.rs
+++ src/card.rs
@@ -1,5 +1,76 @@
 
 #[derive(Copy, Clone)]
 struct Card {
+    suit: Suit,
+    value: Value,
+}
 
+#[derive(Copy, Clone)]
+enum Suit {
+    Spades,
+    Hearts,
+    Diamonds,
+    Clubs,
+}
+
+#[derive(Copy, Clone)]
+enum Color {
+    Black,
+    Red,
+}
+
+#[derive(Copy, Clone)]
+enum Value {
+    Ace,
+    Number(u8),
+    Face(FaceValue)
+}
+
+#[derive(Copy, Clone)]
+enum FaceValue {
+    King,
+    Queen,
+    Jack,
+}
+
+impl Card {
+    fn new(value: Value, suit: Suit) -> Self {
+        Self {
+            value,
+            suit,
+        }
+    }
+}
+
+impl TryFrom<(u8, u8)> for Card {
+    type Error = &'static str;
+
+    fn try_from(value: (u8, u8)) -> Result<Self, Self::Error> {
+        Ok(Self {
+            value: match value.0 {
+                1 => Value::Ace,
+                2_u8..=10_u8 => Value::Number(value.0),
+                11 => Value::Face(FaceValue::Jack),
+                12 => Value::Face(FaceValue::Queen),
+                13 => Value::Face(FaceValue::King),
+                _ => return Err("Invalid card value"),
+            },
+            suit: match value.1 {
+                4 => Suit::Spades,
+                3 => Suit::Hearts,
+                2 => Suit::Diamonds,
+                1 => Suit::Clubs,
+                _ => return Err("Invalid card suit"),
+            }
+        })
+    }
+}
+
+impl Suit {
+    fn get_color(&self) -> Color {
+        match self {
+            Suit::Spades | Suit::Clubs => Color::Black,
+            Suit::Diamonds | Suit::Hearts => Color::Red,
+        }
+    }
 }
