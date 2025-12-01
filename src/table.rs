ce332b1248c7071b3358814c3b537c81e4d2e6b0
Elijah Zimmerman <elijahz0409@gmail.com>
2025-11-30 19:49:53 -0500

    Implement game startup

--- src/table.rs
+++ src/table.rs
@@ -1,13 +1,28 @@
+use std::array::from_fn;
 use crate::pile::Pile;
+use crate::pile::render_type::{Cascade, Flippable, Straight};
 
-pub struct Table {
-    stacks: [Pile; 7], // the general play area
-    home: [Pile; 4],
-    draw: Pile
+pub struct Table<'a> {
+    stacks: [Pile<'a>; 7], // the general play area
+    home: [Pile<'a>; 4],
+    draw: Pile<'a>
 }
 
-impl Table {
+impl Table<'_> {
     pub fn new() -> Self {
-        todo!()
+        let mut deck = Pile::deck52();
+        let mut stacks: [Pile; 7] = from_fn(|i| Pile::new().render_as(Cascade::new(i)));
+
+        for i in 0..7 {
+            for j in i..7 {
+                stacks[j].place_top(deck.draw());
+            }
+        }
+
+        Self {
+            stacks,
+            home: from_fn(|_| Pile::new().render_as(Straight)),
+            draw: deck.render_as(Flippable),
+        }
     }
 }
