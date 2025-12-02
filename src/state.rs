1bdee046d34093ec561972f0460af5bac20cbb75
Elijah Zimmerman <elijahz0409@gmail.com>
2025-12-02 18:05:24 -0500

    Document State module

--- src/state.rs
+++ src/state.rs
@@ -1,17 +1,40 @@
 use std::io;
 use ratatui::{DefaultTerminal, Frame};
 
+/// Application state and event handling
+///
+/// Provides a wrapper around [ratatui].
+///
+/// Example
+/// ```rust
+/// let mut terminal = ratatui::init();
+/// let mut state = State::new();
+/// state.run(&terminal)?;
+/// state.restore();
+/// # Ok::<(), io::Error>(())
+/// ```
 pub struct State {
     exit: bool,
 }
 
 impl State {
+    /// Initialize state.
+    ///
+    /// Example
+    /// ```rust
+    /// let mut state = State::new();
+    /// ```
     pub fn new() -> Self {
         Self {
             exit: false,
         }
     }
-    
+
+    /// Enter into rendering and event handling loop.
+    ///
+    /// Use [ratatui::init()] to get the [DefaultTerminal].
+    ///
+
     pub fn run(&mut self, terminal: &mut DefaultTerminal) -> io::Result<()> {
         while !self.exit {
             terminal.draw(|frame| self.draw(frame))?;
@@ -21,16 +44,17 @@ impl State {
         Ok(())
     }
 
-    pub fn draw(&self, frame: &mut Frame) {
+    /// Frame rendering helper.
     fn draw(&self, frame: &mut Frame) {
         todo!()
     }
 
-    pub fn handle_events(&mut self) -> io::Result<()> {
+    /// Event handling helper.
     fn handle_events(&mut self) -> io::Result<()> {
         todo!()
     }
 
+    /// Restore the terminal to its initial state.
     pub fn restore() {
         ratatui::restore();
     }
