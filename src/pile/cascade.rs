644cbf989b6f9503c31be96d2e6981b7a88a0f4b
Elijah Zimmerman <elijahz0409@gmail.com>
2025-12-04 16:28:23 -0500

    Write documentation for Cascade

--- src/pile/cascade.rs
+++ src/pile/cascade.rs
@@ -3,11 +3,30 @@ use ratatui::buffer::Buffer;
 use ratatui::layout::Rect;
 use crate::pile::{Pile, Renderer};
 
+/// Draws a pile as partially overlapping each other vertically,
+/// displaying the true number of cards in the pile.
+/// The first `n_hidden` cards from the bottom are displayed as card backs,
+/// while the remainder are shown with value and suit.
+///
+/// ```text
+/// ┌─────────┐
+/// │▓▒▓▒▓▒▓▒▓│
+/// ┌─────────┐
+/// │A♠       │
+/// ┌─────────┐
+/// │K♥       │
+/// |         |
+/// |         |
+/// |         |
+/// │       K♥│
+/// └─────────┘
+/// ```
 pub struct Cascade {
     n_hidden: usize,
 }
 
 impl Cascade {
+    /// Construct a new [Cascade] rendering strategy with a given `n_hidden`.
     pub fn new(n_hidden: usize) -> Self {
         Self {
             n_hidden,
