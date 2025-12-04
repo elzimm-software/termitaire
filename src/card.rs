a441b460bfbd98b220eacb30109877739bb7ff57
Elijah Zimmerman <elijahz0409@gmail.com>
2025-12-04 03:57:19 -0500

    Write documentation for Card

--- src/card.rs
+++ src/card.rs
@@ -1,4 +1,9 @@
 
+/// Representation of a playing card.
+///
+/// The primary duty of this struct is to handle comparisons between [cards](Card).
+/// I haven't yet decided if I want [cards](Card) to know how to render themselves
+/// or if I can leave that to the [Pile](crate::pile::Pile) rendering strategies.
 #[derive(Copy, Clone, Debug, PartialEq, Eq, Ord, PartialOrd)]
 pub struct Card {
     suit: Suit,
@@ -6,6 +11,9 @@ pub struct Card {
 }
 
 impl Card {
+    /// Construct a new [Card] with [Value] and [Suit].
+    ///
+    /// See also [try_from()](Card::try_from()) for constructing from integers.
     pub fn new(value: Value, suit: Suit) -> Self {
         Self {
             value,
@@ -14,6 +22,13 @@ impl Card {
     }
 }
 
+/// Construct a [Card] from a [tuple](tuple) of [u8]s
+/// where the first value is the [value](Value) and the second is the [suit](Suit).
+///
+/// [Card] uses the errable [TryFrom] as [u8] values outside [1, 13] are invalid.
+//
+// I've considered implementing TryFrom<u8> for both Value and Suit and using that here,
+// but trying to pass Err up with `?` gets wierd and I see no reason not to do it this way.
 impl TryFrom<(u8, u8)> for Card {
     type Error = &'static str;
 
@@ -38,6 +53,7 @@ impl TryFrom<(u8, u8)> for Card {
     }
 }
 
+/// Enum of possible card suits.
 #[derive(Copy, Clone, Debug, PartialEq, Eq, Ord, PartialOrd)]
 pub enum Suit {
     Spades,
@@ -47,6 +63,7 @@ pub enum Suit {
 }
 
 impl Suit {
+    /// Returns the color of the suit.
     fn get_color(&self) -> Color {
         match self {
             Suit::Spades | Suit::Clubs => Color::Black,
@@ -55,12 +72,18 @@ impl Suit {
     }
 }
 
+/// Enum of possible colors for a suit.
+///
+/// This does not implement [Eq] or [Ord] because all comparisons should be done via [Suit].
 #[derive(Copy, Clone, Debug)]
 pub enum Color {
     Black,
     Red,
 }
 
+/// Enum of possible card values.
+///
+/// [Number](Value::Number) is used for all cards [2, 10].
 #[derive(Copy, Clone, Debug, PartialEq, Eq, Ord, PartialOrd)]
 pub enum Value {
     Ace,
@@ -71,6 +94,11 @@ pub enum Value {
 }
 
 impl Value {
+    /// Returns if the card is a face card.
+    /// 
+    /// [Ace](Value::Ace) is not considered a face card.
+    //
+    // Maybe Ace as a face card could be a feature.
     fn is_face(&self) -> bool {
         match self {
             Value::Jack | Value::Queen | Value::King => true,
